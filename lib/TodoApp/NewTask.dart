66e344154a360373db57bca600260145a9668c65
moooo.kh11@gmail.com <moooo.kh11@gmail.com>
2021-12-19 08:29:39 +0200

    database done

--- lib/TodoApp/NewTask.dart
+++ lib/TodoApp/NewTask.dart
@@ -1,7 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_session1/TodoApp/THomeScreen.dart';
 class NewTasks extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('NewTasks'),);
+    return ListView.separated(
+      itemCount: list.length,
+      separatorBuilder: (context,index){
+        return  Container(height: 1.0,color: Colors.grey,width: 60,);
+      },
+
+        itemBuilder: (context,index){
+      return Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            CircleAvatar
+              (
+              radius: 40,
+              child: Text('${list[index]['id'].toString()}'),
+            ),
+            Column(
+              children: [
+                Text('${list[index]['title'].toString()}'),
+                Text('${list[index]['date'].toString()}'),
+              ],
+            )
+          ],
+        ),
+      );
+    });
   }
 }
--- lib/TodoApp/THomeScreen.dart
+++ lib/TodoApp/THomeScreen.dart
@@ -1,31 +1,43 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_session1/TodoApp/ArchiveTaasks.dart';
 import 'package:flutter_session1/TodoApp/DoneTasks.dart';
 import 'package:flutter_session1/TodoApp/NewTask.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:intl/intl.dart';
+
+
 class THomePage extends StatefulWidget {
   @override
   _THomePageState createState() => _THomePageState();
 }
-
+List<Map> list=[];
 class _THomePageState extends State<THomePage> {
-  Database? database ;
-  List<Widget> screens= [
+  Database? database;
+
+
+  var key = GlobalKey <ScaffoldState>();
+  var fkey = GlobalKey <FormState>();
+  var title = TextEditingController();
+  var time = TextEditingController();
+  var date = TextEditingController();
+
+  List<Widget> screens = [
     NewTasks(),
-  DoneTasks(),
+    DoneTasks(),
     ArchiveTasks()
   ];
-  List<String> appbarnames =[
+  List<String> appbarnames = [
     'New Tasks',
     'Done Tasks',
     'Archived Tasks'
 
   ];
-  int screenindex=0;
+  int screenindex = 0;
 
   @override
   void initState() {
-
     // TODO: implement initState
     super.initState();
     createDataBase();
@@ -35,44 +47,163 @@ class _THomePageState extends State<THomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-       title: Text(appbarnames[screenindex]),
+        title: Text(appbarnames[screenindex]),
       ),
+      key: key,
       body: screens[screenindex],
-      floatingActionButton: FloatingActionButton(onPressed: (){insertdata();},child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        key.currentState!.showBottomSheet((context) =>
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.all(50),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Form(
+                        key: fkey,
+                        child: Column(
+
+                          children: [
+                            TextFormField(
+                              controller: title,
+                              validator: (value) {
+                                if (value == null) {
+                                  return 'Please enter some text';
+                                }
+                              },
+                              onFieldSubmitted: (value) {
+                                title.text = value;
+                              },
+                              decoration: InputDecoration(
+                                labelText: "Title",
+                                border: OutlineInputBorder(),
+                                prefixIcon: Icon(Icons.title),
+
+                              ),
+
+                            ),
+                            SizedBox(height: 10,),
+                            TextFormField(
+                              keyboardType: TextInputType.datetime,
+                              controller: time,
+                              validator: (value) {
+                                if (value == null) {
+                                  return 'Please enter time';
+                                }
+                              },
+                              onTap: () {
+                                showTimePicker(
+                                    context: context, initialTime: TimeOfDay.now())
+                                    .then((value) =>
+                                time.text = value!.format(context));
+                              },
+
+                              onChanged: (value) {
+                                time.text = value;
+                              },
+                              decoration: InputDecoration(
+                                labelText: "Title",
+                                border: OutlineInputBorder(),
+                                prefixIcon: Icon(Icons.watch_later),
+
+                              ),
+
+                            ),
+                            SizedBox(height: 10,),
+                            TextFormField(
+                              keyboardType: TextInputType.datetime,
+                              controller: date,
+                              validator: (value) {
+                                if (value == null) {
+                                  return 'Please enter Date';
+                                }
+                              },
+                              onTap: () {
+                                showDatePicker(context: context,
+                                    initialDate: DateTime.now(),
+                                    firstDate: DateTime.now(),
+                                    lastDate: DateTime.parse('2022-05-03')).then((value) =>date.text=DateFormat.yMMMd().format(value!) );
+                              },
+
+                              onChanged: (value) {
+                                date.text = value;
+                              },
+                              decoration: InputDecoration(
+                                labelText: "Date",
+                                border: OutlineInputBorder(),
+                                prefixIcon: Icon(Icons.calendar_today),
+
+                              ),
+
+                            ),
+
+                          ],
+                        ),
+                      ),
+                      ElevatedButton(onPressed: () {
+                        if (fkey.currentState!.validate()) {
+                          insertdata(title: title.text,time: time.text,date: date.text);
+                          print('Valid');
+                          Navigator.pop(context);
+                        }
+                      }, child: Text('ADD')),
+                    ],
+                  ),
+                ),
+              ),
+
+            )).close();
+      }, child: Icon(Icons.add),),
       bottomNavigationBar: BottomNavigationBar(
 
         currentIndex: screenindex,
-        onTap: (index){
-
+        onTap: (index) {
           setState(() {
-            screenindex=index;
+            screenindex = index;
           });
         },
         items: [
-        BottomNavigationBarItem(icon: Icon(Icons.menu_book),label: 'NewTask'),
-          BottomNavigationBarItem(icon: Icon(Icons.done),label: 'DoneTasks'),
-          BottomNavigationBarItem(icon: Icon(Icons.archive),label: 'ArchiveTask'),
-      ],),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.menu_book), label: 'NewTask'),
+          BottomNavigationBarItem(icon: Icon(Icons.done), label: 'DoneTasks'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.archive), label: 'ArchiveTask'),
+        ],),
     );
   }
-  void createDataBase() async{
 
+  void createDataBase() async {
     database = await openDatabase(
         'todo.db',
         version: 1,
-        onCreate: (database,version){
+        onCreate: (database, version) {
           print('database created');
-          database.execute('CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT ,date TEXT,time TEXT,type TEXT)').then((value) => print("Table created "));
+          database.execute(
+              'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT ,date TEXT,time TEXT,type TEXT)')
+              .then((value) => print("Table created "));
         },
-        onOpen: (database){print('database opend');}
+        onOpen: (database) {
+          RetriveRaws(database).then((value) {list=value;
+          print(list[0]);
+          print('database opend');
+          });
+
+        }
     );
   }
-  void insertdata(){
-    database!.transaction((txn) async {
-      await txn.rawInsert('INSERT INTO tasks(title,date,time,type) Values("first task","2020","10","new")').then((value) => print('added'));
-    }
-    );}
-
+   Future <List<Map>> RetriveRaws(database)  async {
+   return await database.rawQuery('SELECT * FROM tasks');
   }
+  Future? insertdata({required String title,required String time,required String date}) {
+    database!.transaction((txn) async {
+      return await txn.rawInsert(
+          'INSERT INTO tasks(title,date,time,type) Values("$title","$time","$date","new")')
+          .then((value) => print('added'));
+
+    });
+       return null;
+  }
+
+}
 
 
